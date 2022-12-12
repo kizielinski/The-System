8ebd878e3d09aea2773a305e2ebd113492d66ee5
meideka20 <70358214+meideka20@users.noreply.github.com>
2022-12-12 16:58:22 -0500

    enemy exists and can walk :D

--- TheSystem/Assets/Scripts/Enemies/Walker.cs
+++ TheSystem/Assets/Scripts/Enemies/Walker.cs
@@ -4,22 +4,83 @@ using UnityEngine;
 
 public class Walker : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private LayerMask playerLayerMask;
+
     public bool facingRight;
     public float walkSpeed;
 
+    private BoxCollider2D collisionBox;
+
+    private Vector2 pos;
+
     void Start()
     {
-        
+        collisionBox = GetComponent<BoxCollider2D>();
+
+        pos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Walk();
+        transform.position = pos;
     }
 
-    private bool IsWall()
+    void Walk()
     {
-        return facingRight;
+        if(facingRight)
+        {
+            pos.x += walkSpeed * Time.deltaTime;
+        }
+        else
+        {
+            pos.x -= walkSpeed * Time.deltaTime;
+        }
+        if(HitWall())
+        {
+            TurnAround();
+        }
+    }
+
+    public bool HitWall()
+    {
+        RaycastHit2D hit;
+        if (facingRight)
+        {
+            hit = Physics2D.Raycast(collisionBox.bounds.center, Vector2.right, collisionBox.bounds.extents.x + .01f, platformLayerMask);
+        }
+        else
+        {
+            hit = Physics2D.Raycast(collisionBox.bounds.center, Vector2.left, collisionBox.bounds.extents.x + .01f, platformLayerMask);
+        }
+        Color rayColor;
+        if (hit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.blue;
+        }
+        if (facingRight)
+        {
+            Debug.DrawRay(collisionBox.bounds.center, Vector2.right * (collisionBox.bounds.extents.x + .01f), rayColor);
+        }
+        else
+        {
+            Debug.DrawRay(collisionBox.bounds.center, Vector2.left * (collisionBox.bounds.extents.x + .01f), rayColor);
+        }
+        return hit.collider != null;
+    }
+
+    void TurnAround()
+    {
+        if(facingRight)
+        {
+            facingRight = false;
+            return;
+        }
+        facingRight = true;
     }
 }
