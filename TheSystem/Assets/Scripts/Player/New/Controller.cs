94417940d304c762488fddb85b52eb5e83621138
meideka20 <70358214+meideka20@users.noreply.github.com>
2022-12-16 00:27:33 -0500

    Commented everything

--- TheSystem/Assets/Scripts/Player/New/Controller.cs
+++ TheSystem/Assets/Scripts/Player/New/Controller.cs
@@ -5,84 +5,127 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Controller : MonoBehaviour
 {
+    // Getting the proper layer that the player is supposed to collide with
     public LayerMask collisionMask;
 
-    const float skinWidth = .015f;
+    // Raycasting values
+    const float skinWidth = .015f; // How far into the object the rays start
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
-
-    float maxClimbAngle = 60;
-
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
+    // The maximum angle of a slope that the player can climb
+    float maxClimbAngle = 60;
+
+    // Player collision info
     BoxCollider2D playerCollider;
+
+    // Raycast struct definition
     RaycastOrigins raycastOrigins;
+
+    // Collision info struct definition
     public CollisionInfo collisions;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Get's the player's collision box and calculates the ray spacing for the raycasting
+    /// </summary>
     void Start()
     {
         playerCollider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
     }
 
+    /// <summary>
+    /// Checks for collisions horizontally and verically
+    /// before moving the player to it's expected location
+    /// </summary>
+    /// <param name="velocity">The player's current velocity after user input</param>
     public void Move(Vector3 velocity)
     {
+        // Updates the four corners of the raycast box
         UpdateRaycastOrigins();
+
+        // Resets the current collision states
         collisions.Reset();
 
+        // Check for horizontal collisions if the player is moving horizontally
         if (velocity.x != 0)
         {
             HorizontalCollisions(ref velocity);
         }
+        // Check for vertical collisions if the player is moving vertically
         if (velocity.y != 0)
         {
             VerticalCollisions(ref velocity);
         }
 
+        // Moves the player after necessary alterations are done to the velocity
         transform.Translate(velocity);
     }
 
+    /// <summary>
+    /// Checks for horizontal collisions
+    /// Determines whether or not the slope of the object that is collided with is climbable
+    ///     and climbs the slope without losing horizontal speed
+    /// </summary>
+    /// <param name="velocity">The player's current velocity after user input</param>
     void HorizontalCollisions(ref Vector3 velocity)
     {
+        // Gets the direction of the movement
         float directionX = Mathf.Sign(velocity.x);
+        // Gets the length of the ray necessary to check for collisions
         float rayLength = Mathf.Abs(velocity.x) + skinWidth;
 
+        // Loops through rays to cast looking for a collision
         for (int i = 0; i < verticalRayCount; i++)
         {
+            // Sets the origin of the first ray to be on the left or right depending on what direction the player is facing
             Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            // Offsets each ray by the correct spacing amount
             rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            // Casts a ray and stores the result of the cast
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
 
+            // Draws the rays being cast
             Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
 
+            // Steps to take after a raycast has hit
             if (hit)
             {
+                // Gets the angle of the hit object
                 float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
 
+                // Checking if the player can climb the slope
                 if(i == 0 && slopeAngle <= maxClimbAngle)
                 {
+                    // checking if the slope climb was started last frame
                     float distanceToSlopeStart = 0;
                     if(slopeAngle != collisions.slopeAngleOld)
                     {
+                        // Brings the player to be touching the slope
                         distanceToSlopeStart = hit.distance - skinWidth;
                         velocity.x -= distanceToSlopeStart * directionX;
                     }
+                    // Performs calculations to climb the slope
                     ClimbSlope(ref velocity, slopeAngle);
                     velocity.x += distanceToSlopeStart * directionX;
                 }
 
+                // Things to do when the player can't climb the new slope
                 if (!collisions.climbingSlope || slopeAngle > maxClimbAngle)
                 {
+                    // Stop the player from going through the wall
                     velocity.x = (hit.distance - skinWidth) * directionX;
                     rayLength = hit.distance;
 
+                    // If the player was climbing a slope offset the vertical movement so it doesn't bounce
                     if (collisions.climbingSlope)
                     {
                         velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
                     }
 
+                    // Keep track of what side of the player the collision is on
                     collisions.left = directionX == -1;
                     collisions.right = directionX == 1;
                 }
@@ -90,40 +133,63 @@ public class Controller : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Checks for Vertical collisions
+    /// </summary>
+    /// <param name="velocity">The player's current velocity after user input</param>
     void VerticalCollisions(ref Vector3 velocity)
     {
+        // Gets the direction of movement
         float directionY = Mathf.Sign(velocity.y);
+        // Gets the length of the ray necessary to check for collisions
         float rayLength = Mathf.Abs(velocity.y) + skinWidth;
 
+        // Casts rays and performs logic based on whether or not there was a hit
         for (int i = 0; i < verticalRayCount; i++)
         {
+            // Sets the origin of the first ray to be on the top or bottom depending on what direction the player is moving
             Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+
+            // Casts a ray and stores the hit value
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
 
+            // Draws the cast rays
             Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
 
+            // Logic to perform on a hit
             if (hit)
             {
+                // Stop the player right before they go through the object
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
 
+                // If the player was climbing a slope offset the horizontal movement so it stays against the slope
                 if (collisions.climbingSlope)
                 {
                     velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
                 }
 
+                // Keep track of what side of the player the collision was on
                 collisions.below = directionY == -1;
                 collisions.above = directionY == 1;
             }
         }
     }
 
+    /// <summary>
+    /// Performs the calculations to walk up a slope at the same horizontal speed as flat terrain
+    /// </summary>
+    /// <param name="velocity"></param>
+    /// <param name="slopeAngle"></param>
     void ClimbSlope(ref Vector3 velocity, float slopeAngle)
     {
+        // Determines the total horizontal distance it is supposed to move up the slope
         float moveDistance = Mathf.Abs(velocity.x);
+        // Calculates how far up the slope it has to move in order to be at the correct X position
         float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
 
+        // Checks if the player is not jumping and climbs the slope
         if (velocity.y <= climbVelocityY)
         {
             velocity.y = climbVelocityY;
@@ -134,6 +200,9 @@ public class Controller : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Calculates how much space should be in between each ray
+    /// </summary>
     void CalculateRaySpacing()
     {
         Bounds bounds = playerCollider.bounds;
@@ -146,6 +215,9 @@ public class Controller : MonoBehaviour
         verticalRaySpacing = bounds.size.y / (verticalRayCount - 1);
     }
 
+    /// <summary>
+    /// Updates the four corners of the box where the raycasts will begin drawing from
+    /// </summary>
     void UpdateRaycastOrigins()
     {
         Bounds bounds = playerCollider.bounds;
@@ -157,12 +229,18 @@ public class Controller : MonoBehaviour
         raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
     }
 
+    /// <summary>
+    /// Keeps track of all four corners of the raycasting box
+    /// </summary>
     struct RaycastOrigins
     {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
     }
 
+    /// <summary>
+    /// Keeps tracks of the current collision states
+    /// </summary>
     public struct CollisionInfo
     {
         public bool above, below;
--- TheSystem/Assets/Scripts/Player/New/Player.cs
+++ TheSystem/Assets/Scripts/Player/New/Player.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(Controller))]
 public class Player : MonoBehaviour
 {
+    // Variables that can be set in the inspector related to physics
     public float jumpHeight = 4;
     public float timeToJumpApex = .4f;
     public float accelerationTimeAirborne = .2f;
     public float accelerationTimeGrounded = .1f;
     public float moveSpeed = 6;
 
+    // Calculated values based on the variables above
     float gravity;
     float jumpVelocity;
     Vector3 velocity;
     float velocityXSmoothing;
 
+    // Reference to the controller script
     Controller controller;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Gets a reference to the Controller script and calculates the gravity and jumpVelocity values
+    /// </summary>
     void Start()
     {
         controller = GetComponent<Controller>();
@@ -27,23 +32,31 @@ public class Player : MonoBehaviour
         jumpVelocity = Mathf.Abs(gravity * timeToJumpApex);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Handles player input
+    /// </summary>
     void Update()
     {
+        // Resets the vertical velocity if the player touches a ceiling or floor
         if(controller.collisions.above || controller.collisions.below)
         {
             velocity.y = 0;
         }
 
+        // Gets the current player axis input
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        // Checks if the player performed a jump
         if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
         {
             velocity.y = jumpVelocity;
         }
 
+        // Smooths out horizontal movement
         float targetVelocityX = input.x * moveSpeed;
         velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+        // Updates the player's velocity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
