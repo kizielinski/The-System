deb0311668ef4b600ac6a4f24074353aa68a869f
Collinb5101 <collinb5101@gmail.com>
2023-02-18 20:38:54 -0500

    temporary shitty grapple, WILL NOT STAY

--- TheSystem/Assets/Scripts/Player/Grappler.cs
+++ TheSystem/Assets/Scripts/Player/Grappler.cs
@@ -4,20 +4,56 @@ using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
-    LineRenderer grapple;
-    DistanceJoint2D grappleJoint;
-    Vector2 grappleStart;
+    public LineRenderer grapple;
+
+    float distanceX;
+    float distanceY;
+    Vector2 mousPos;
+    //temporary test case, will be removed in final 
+    public Camera cameraObj;
+    //will be used in the final version instead of mouse
+    //Vector2 grappleStart;
     bool isScrapper;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            mousPos = (Vector2)cameraObj.ScreenToWorldPoint(Input.mousePosition);
+            grapple.SetPosition(0, mousPos);
+            grapple.SetPosition(1, transform.position);
+            
+            distanceX = (transform.position.x - mousPos.x);
+            distanceY = Mathf.Pow((transform.position.y - mousPos.y), 2);
+            grapple.enabled = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            grapple.enabled = false;
+        }
+
+        if(grapple.enabled)
+        {
+            grapple.SetPosition(1, transform.position);
+
+            if(transform.position.x > distanceX)
+            {
+                transform.position = new Vector2(distanceX, transform.position.y);
+            }
+            if(transform.position.x < -distanceX)
+            {
+                transform.position = new Vector2(-distanceX, transform.position.y);
+            }
+           // if (transform.position.y > distanceY)
+           // {
+           //     transform.position = mousPos + new Vector2(distanceX, distanceY);
+            //}
+        }
     }
 }
