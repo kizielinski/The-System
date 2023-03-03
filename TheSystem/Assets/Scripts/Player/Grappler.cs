59eaa318831208646cd7caba0f141a7373c5d9b2
Collinb5101 <collinb5101@gmail.com>
2023-03-02 20:27:46 -0500

    Grapple is now in Grappler script

--- TheSystem/Assets/Scripts/Player/Grappler.cs
+++ TheSystem/Assets/Scripts/Player/Grappler.cs
@@ -4,25 +4,141 @@ using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
+    public Player player;
     public LineRenderer grapple;
 
-    float distanceX;
-    float distanceY;
-    Vector2 mousPos;
-    //temporary test case, will be removed in final 
-    public Camera cameraObj;
-    //will be used in the final version instead of mouse
-    //Vector2 grappleStart;
-    bool isScrapper;
+    public bool isGrappleOn;
+
+    public float newPointX;
+    public float newPointY;
+    public float zipSpeedX;
+    public float zipSpeedY;
+
+    Vector2 newPos;
+    float distance;
 
     // Start is called before the first frame update
     void Start()
     {
+        grapple.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if its been allowed to work
+        if (isGrappleOn)
+        {
+            //if you click
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+
+                //calculate where the new position is based on the player and direction faced
+                newPos = transform.position + new Vector3(newPointX * Mathf.Sign(player.Velocity.x), newPointY);
+
+
+
+                //set the line positions to draw
+                grapple.SetPosition(0, newPos);
+                grapple.SetPosition(1, transform.position);
+
+                //calculate the distance between the player position and the new position
+                distance = Mathf.Sqrt(Mathf.Pow((transform.position.x - newPos.x), 2) + Mathf.Pow((transform.position.y - newPos.y), 2));
+
+                //draw the line renderer
+                grapple.enabled = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                //if you release mouse1 then stop drawing the line
+                grapple.enabled = false;
+            }
+
+            //if the line is being drawn
+            if (grapple.enabled)
+            {
+                //set the position
+                grapple.SetPosition(1, transform.position);
+
+                //calculate a zip vector based on position and new position
+                Vector3 zipVector = new Vector3(0, 0, 0) - transform.localPosition + (Vector3)newPos;
+
+
+                
+
+                //if the player is grounded
+                if (player.Controller.collisions.below)
+                {
+                    //bump them up a little bit because the grapple wasnt a fan of the player being stuck to the ground
+                    player.transform.position += new Vector3(0, 0.01f);
+                }
+                //if the player has reached the new position (close enough to it)
+                if (Vector2.Distance(transform.position, newPos) < distance / 10)
+                {
+                    //set their position to the new position and zero out their velocity
+                    transform.position = newPos;
+                    player.Velocity = new Vector3(0, 0);
+                }
+                else
+                {
+                    //otherwise just keep adding velocities based on the calculated direction (zipvector)
+                    player.Velocity += new Vector3((zipSpeedX * zipVector.x * Time.deltaTime), (zipSpeedY * zipVector.y * Time.deltaTime));
+
+                    /* other possible way of zipping to point you can mess with in the y direction, 
+                     * decide which one is best
+                    velocity.y = 0;
+                    velocity.y += (1000 * zipVector.y * Time.deltaTime);
+                    */
+                }
+                //currently drawing the ray instead of the linerenderer for the sake of making sure zip rays would be correct
+                Debug.DrawRay(transform.position, zipVector);
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+/* the old grapple that i worked on that didnt make the cut i'm still mad about that if you're reading
+ * this Karl I hate you just kidding you're my boss i love you i understand why it didn't make it 
+ * please god don't fire me hahahahah
+
+//temporary grapple code i needed velocity sorry
+if(isGrappleOn)
+{
+    if (Input.GetKeyDown(KeyCode.Mouse0))
+    {
+        newPos = (Vector2)cameraObj.ScreenToWorldPoint(Input.mousePosition);
+        grapple.SetPosition(0, newPos);
+        grapple.SetPosition(1, transform.position);
+        distance = Mathf.Sqrt(Mathf.Pow((transform.position.x - newPos.x), 2) + Mathf.Pow((transform.position.y - newPos.y), 2));
+
+        grapple.enabled = true;
+    }
+    else if (Input.GetKeyUp(KeyCode.Mouse0))
+    {
+        grapple.enabled = false;
+    }
+
+    if (grapple.enabled)
+    {
+        grapple.SetPosition(1, transform.position);
+
+        if (Vector2.Distance(transform.position, newPos) > distance)
+        {
+            velocity.x -= 20 * (transform.position.x - newPos.x) * Time.deltaTime;
+        }
+
+        if (Vector2.Distance(transform.position, newPos) > distance)
+        {
+            velocity.y -= 20 * (transform.position.y - newPos.y) * Time.deltaTime;
+        }
     }
 }
+*/
+
--- TheSystem/Assets/Scripts/Player/Player.cs
+++ TheSystem/Assets/Scripts/Player/Player.cs
@@ -29,27 +29,23 @@ public class Player : MonoBehaviour
 
 
 
-    //temporary grapple stuff sorry
-    public LineRenderer grapple;
-    public bool isGrappleOn;
-    public float newPointX;
-    public float newPointY;
-    public float zipSpeedX;
-    public float zipSpeedY;
-    float distance;
-    Vector2 newPos;
-    RaycastHit2D hit;
-    Ray zipRay;
-    //temporary test case, will be removed in final 
-    public Camera cameraObj;
+    
 
 
-
-
-    //velocity getter
+    //velocity getter/setter
     public Vector3 Velocity
     {
         get { return velocity; }
+        set { 
+            velocity.x = value.x;
+            velocity.y = value.y;
+        }
+    }
+
+    //controller getter
+    public PlayerController Controller
+    {
+        get { return controller; }
     }
 
     // Reference to the controller script
@@ -70,7 +66,7 @@ public class Player : MonoBehaviour
         {
             instance = this;
         }
-        grapple.enabled = false;
+
         //gets the script
         grabber = GetComponent<LedgeGrabber>();
         slider = GetComponent<WallSlide>();
@@ -125,128 +121,6 @@ public class Player : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
 
 
-        /* the old grapple that i worked on that didnt make the cut i'm still mad about that if you're reading
-         * this Karl I hate you just kidding you're my boss i love you i understand why it didn't make it 
-         * please god don't fire me hahahahah
-          
-        //temporary grapple code i needed velocity sorry
-        if(isGrappleOn)
-        {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                newPos = (Vector2)cameraObj.ScreenToWorldPoint(Input.mousePosition);
-                grapple.SetPosition(0, newPos);
-                grapple.SetPosition(1, transform.position);
-                distance = Mathf.Sqrt(Mathf.Pow((transform.position.x - newPos.x), 2) + Mathf.Pow((transform.position.y - newPos.y), 2));
-
-                grapple.enabled = true;
-            }
-            else if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                grapple.enabled = false;
-            }
-
-            if (grapple.enabled)
-            {
-                grapple.SetPosition(1, transform.position);
-
-                if (Vector2.Distance(transform.position, newPos) > distance)
-                {
-                    velocity.x -= 20 * (transform.position.x - newPos.x) * Time.deltaTime;
-                }
-
-                if (Vector2.Distance(transform.position, newPos) > distance)
-                {
-                    velocity.y -= 20 * (transform.position.y - newPos.y) * Time.deltaTime;
-                }
-            }
-        }
-        */
-
-        //if its been allowed to work
-        if (isGrappleOn)
-        {
-            //if you click
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                
-                //calculate where the new position is based on the player and direction faced
-                newPos = transform.position + new Vector3(newPointX * Mathf.Sign(velocity.x), newPointY);
-                
-
-
-                //set the line positions to draw
-                grapple.SetPosition(0, newPos);
-                grapple.SetPosition(1, transform.position);
-
-                //calculate the distance between the player position and the new position
-                distance = Mathf.Sqrt(Mathf.Pow((transform.position.x - newPos.x), 2) + Mathf.Pow((transform.position.y - newPos.y), 2));
-
-                //draw the line renderer
-                grapple.enabled = true;
-            }
-            else if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                //if you release mouse1 then stop drawing the line
-                grapple.enabled = false;
-            }
-
-            //if the line is being drawn
-            if (grapple.enabled)
-            { 
-                //set the position
-                grapple.SetPosition(1, transform.position);
-
-                //calculate a zip vector based on position and new position
-                Vector3 zipVector = new Vector3(0, 0, 0)-transform.localPosition + (Vector3)newPos;
-                
-                
-                /* dis shit fuckin sucks bruh
-                zipRay = new Ray(transform.position, zipVector);
-                Debug.DrawRay(transform.position, zipVector);
-                if (Physics2D.Raycast(zipRay.origin, zipVector))
-                {
-                    hit = (Physics2D.Raycast(zipRay.origin, zipVector));
-                    Debug.Log(hit.collider);
-                    if (hit.collider.tag == "ground")
-                    {
-                        newPos.x = Mathf.Cos(hit.distance);
-                        newPos.y = Mathf.Sin(hit.distance);
-                    }
-                }
-                */
-
-               //if the player is grounded
-                if(controller.collisions.below)
-                {
-                    //bump them up a little bit because the grapple wasnt a fan of the player being stuck to the ground
-                    transform.position += new Vector3(0, 0.01f);
-                }
-                //if the player has reached the new position (close enough to it)
-                if(Vector2.Distance(transform.position, newPos)<distance/10)
-                {
-                    //set their position to the new position and zero out their velocity
-                    transform.position = newPos;
-                    velocity.x = 0;
-                    velocity.y = 0;
-                }
-                else
-                {
-                    //otherwise just keep adding velocities based on the calculated direction (zipvector)
-                    velocity.x += (zipSpeedX*zipVector.x * Time.deltaTime);
-                    velocity.y += (zipSpeedY*zipVector.y * Time.deltaTime);
-
-                    /* other possible way of zipping to point you can mess with in the y direction, 
-                     * decide which one is best
-                    velocity.y = 0;
-                    velocity.y += (1000 * zipVector.y * Time.deltaTime);
-                    */
-                }
-                //currently drawing the ray instead of the linerenderer for the sake of making sure zip rays would be correct
-                Debug.DrawRay(transform.position, zipVector);
-            }
-            
-        }
 
     }
 
